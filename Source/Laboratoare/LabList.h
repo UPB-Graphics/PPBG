0ccf6b8eace3c3b7031fd62faca9c488bac2016d
Gabriel Ivanica <gabriel.ivanica@gmail.com>
2018-10-23 16:48:13 +0300

    added GeometryShader assignment

--- Source/Laboratoare/LabList.h
+++ Source/Laboratoare/LabList.h
@@ -3,4 +3,5 @@
 #include <Laboratoare/Laborator1/Laborator1.h>
 #include <Laboratoare/SimpleShaders/SimpleShaders.h>
 #include <Laboratoare/OpenGLTextures/OpenGLTextures.h>
+#include <Laboratoare/GeometryShader/GeometryShader.h>
 #include <Laboratoare/FrameBuffers/FrameBuffers.h>
